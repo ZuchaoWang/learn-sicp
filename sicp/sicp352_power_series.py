dcfbcbede952ad736a4069a6669ce7cd0f42da4f
zuchao.wang <zuchao.wang@gmail.com>
2021-07-31 15:29:26 +0800

    update 352 power series

--- sicp/sicp352_power_series.py
+++ sicp/sicp352_power_series.py
@@ -1,66 +1,85 @@
 # exercise 3.59-62
 
 from typing import Callable, Tuple
-from .sicp352_prime_number import Stream
+import sicp352_prime_number
+
+Stream = sicp352_prime_number.Stream
 
 
-class PowerSeries(Stream):
-    def ps_stringify(self, n: int) -> str:
-        if n < 2:
-            n = 2
-        # get topn coefficients
-        coeffs = self.topn_values(n)
-        # calculate coeff_strs, e.g. 1, 2*x, -4*x^2
-        coeff_strs = []
-        if coeffs[0] != 0:
+def almost_equal(x: float, y: float):
+    eps = 1e-6
+    ok1 = abs(x-y) < eps
+    ok2 = (abs(x-y) < eps * (abs(x) + abs(y)))
+    return ok1 or ok2
+
+
+def ps_stringify(s: Stream, n: int) -> str:
+    if n < 2:
+        n = 2
+    # get topn coefficients
+    coeffs = s.topn_values(n)
+    # calculate coeff_strs, e.g. 1, 2*x, -4*x^2
+    coeff_strs = []
+    if not almost_equal(coeffs[0], 0):
+        if not almost_equal(coeffs[0], 1):
             coeff_strs.append(str(coeffs[0]))
-        if coeffs[1] != 0:
+        else:
+            coeff_strs.append('1')
+    if not almost_equal(coeffs[1], 0):
+        if not almost_equal(coeffs[1], 1):
             coeff_strs.append('%f*x' % coeffs[1])
-        for i in range(2, n):
-            if coeffs[i] != 0:
+        else:
+            coeff_strs.append('x')
+    for i in range(2, n):
+        if not almost_equal(coeffs[i], 0):
+            if not almost_equal(coeffs[i], 1):
                 coeff_strs.append('%f*x^%d' % (coeffs[i], i))
-        # if all zeros
-        if len(coeff_strs) == 0:
-            return '0'
-        else:  # has at least one none-zero term
-            res = coeff_strs[0]
-            for cs in coeff_strs[1:]:
-                if cs[0] != '-':
-                    cs = '+' + cs
-                res += cs
-            return res
-
-    def ps_reciprocal(self) -> "PowerSeries":
-        head = self.value()
-        assert head != 0
-        self_normalized = self.scale(1/head)
-        rp = Stream(1, lambda: self_normalized.next().scale(-1).ps_mult(rp))
-        return rp.scale(1/head)
-
-    def ps_square(self) -> "PowerSeries":
-        return PowerSeries.ps_mult(self, self)
-
-    @staticmethod
-    def ps_mult(p1: "PowerSeries", p2: "PowerSeries") -> "PowerSeries":
-        def calc_mult(n: int) -> "PowerSeries":
-            coeffs1 = p1.topn_values(n+1)
-            coeffs2 = p2.topn_values(n+1)
-            sum = 0
-            for i in range(n+1):
-                sum += coeffs1[i] * coeffs2[n+1-i]
-            return PowerSeries(sum, lambda: calc_mult(n+1))
-
-        return calc_mult(0)
-
-    @staticmethod
-    def ps_div(p1: "PowerSeries", p2: "PowerSeries") -> "PowerSeries":
-        return PowerSeries.ps_mult(p1, p2.ps_reciprocal())
+            else:
+                coeff_strs.append('x^%d' % (i))
+    # if all zeros
+    if len(coeff_strs) == 0:
+        return '0'
+    else:  # has at least one none-zero term
+        res = coeff_strs[0]
+        for cs in coeff_strs[1:]:
+            if cs[0] != '-':
+                cs = '+' + cs
+            res += cs
+        return res
 
 
-def make_sin_cos() -> Tuple[PowerSeries, PowerSeries]:
+def ps_mult(s1: Stream, s2: Stream) -> Stream:
+    def calc_mult(n: int) -> Stream:
+        coeffs1 = s1.topn_values(n+1)
+        coeffs2 = s2.topn_values(n+1)
+        sum = 0
+        for i in range(n+1):
+            sum += coeffs1[i] * coeffs2[n-i]
+        return Stream(sum, lambda: calc_mult(n+1))
+
+    return calc_mult(0)
+
+
+def ps_reciprocal(s: Stream) -> Stream:
+    head = s.value()
+    assert head != 0
+    self_normalized = s.scale(1/head)
+    rp = Stream(1, lambda: ps_mult(self_normalized.next().scale(-1), rp))
+    return rp.scale(1/head)
+
+
+def ps_square(s: Stream) -> Stream:
+    return ps_mult(s, s)
+
+
+def ps_div(s1: Stream, s2: Stream) -> Stream:
+    return ps_mult(s1, ps_reciprocal(s2))
+
+
+def make_sin_cos() -> Tuple[Stream, Stream]:
     integers_rp = Stream.make_integers().next().reciprocal()
-    sin = PowerSeries(0, lambda: Stream.mult(cos, integers_rp))
-    cos = PowerSeries(1, lambda: Stream.mult(sin, integers_rp).scale(-1))
+    sin = Stream(0, lambda: Stream.mult(cos, integers_rp))
+    cos = Stream(1, lambda: Stream.mult(sin, integers_rp).scale(-1))
     return sin, cos
 
 
@@ -99,7 +118,7 @@ def make_bernoulli() -> Stream:
 
 
 def coeff_sin(i: int, factorial: Stream) -> float:
-    if i % 2:
+    if i % 2 == 0:
         return 0
     else:
         c = 1/factorial.nth_value(i)
@@ -110,7 +129,7 @@ def coeff_sin(i: int, factorial: Stream) -> float:
 
 
 def coeff_cos(i: int, factorial: Stream) -> float:
-    if i % 2:
+    if i % 2 == 0:
         c = 1/factorial.nth_value(i)
         if i % 4 == 0:
             return c
@@ -143,21 +162,23 @@ def coeff_tan(i: int, factorial: Stream, bernoulli: Stream, exponential2: Stream
             return -c
 
 
-def test_one(name: str, p: PowerSeries, n_coeff: int, coeff_func: Callable[[int], float]):
-    p_str = p.ps_stringify(n_coeff)
+def test_one(name: str, s: Stream, n_coeff: int, coeff_func: Callable[[int], float]):
+    p_str = ps_stringify(s, n_coeff)
     print('%s(x) = %s' % (name, p_str))
     for i in range(n_coeff):
-        pn = p.nth_value(i)
+        pn = s.nth_value(i)
         cn = coeff_func(i)
-        assert abs(pn - cn) <= 1e-4 * (abs(pn) + abs(cn))
+        ok = almost_equal(pn, cn)
+        if not ok:
+            print(name, i, pn, cn)
+            assert False
 
 
 def test():
     # calculate
     ps_sin, ps_cos = make_sin_cos()
-    ps_one = PowerSeries.add(PowerSeries.square(
-        ps_sin), PowerSeries.ps_square(ps_cos))
-    ps_tan = PowerSeries.ps_div(ps_sin, ps_cos)
+    ps_one = Stream.add(ps_square(ps_sin), ps_square(ps_cos))
+    ps_tan = ps_div(ps_sin, ps_cos)
     # test prepare
     factorial = make_factorial()
     bernoulli = make_bernoulli()
@@ -169,3 +190,7 @@ def test():
     test_one("one", ps_one, n_coeff, lambda i: coeff_one(i))
     test_one("tan", ps_tan, n_coeff, lambda i: coeff_tan(
         i, factorial, bernoulli, exponential2))
+
+
+if __name__ == '__main__':
+    test()
