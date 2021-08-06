98deadfb4620af376a763e11da202a969523d111
ZuchaoWang <zuchao.wang@gmail.com>
2021-08-06 16:23:14 +0800

    update 417 analyzer

--- src/sicp417_evaluator.py
+++ src/sicp417_evaluator.py
@@ -9,8 +9,7 @@
 # finally evaluator executes the functions
 
 import sys
-import enum
-from typing import List, Optional
+from typing import Callable, Dict, List, Optional
 
 
 def panic(msg: str):
@@ -31,113 +30,117 @@ def stringify_number(x: float):
 # ref: https://craftinginterpreters.com/scanning.html
 
 
-@enum.unique
-class TokenType(enum.Enum):
-    LEFT_PAREN = enum.auto()
-    RIGHT_PAREN = enum.auto()
-    QUOTE = enum.auto()
-    SYMBOL = enum.auto()
-    NUMBER = enum.auto()
-    STRING = enum.auto()
+LEFT_PAREN = 'LEFT_PAREN'
+RIGHT_PAREN = 'RIGHT_PAREN'
+QUOTE = 'QUOTE'
+SYMBOL = 'SYMBOL'
+NUMBER = 'NUMBER'
+STRING = 'STRING'
 
 
 class Token:
-    def __init__(self, token_type: TokenType, lexeme: str, literal):
-        self.token_type = token_type
+    def __init__(self, typ: str, lexeme: str, literal):
+        self.typ = typ
         self.lexeme = lexeme
         self.literal = literal
 
-    def stringify(self):
-        if isinstance(self.literal, float):
-            return '%s:%s' % (self.token_type.name, stringify_number(self.literal))
-        elif isinstance(self.literal, str):
-            return '%s:%s' % (self.token_type.name, self.literal)
+
+class TokenPrintier:
+    def stringify(self, token: Token):
+        if isinstance(token.literal, float):
+            return '%s:%s' % (token.typ, stringify_number(token.literal))
+        elif isinstance(token.literal, str):
+            return '%s:%s' % (token.typ, token.literal)
         else:
-            return self.token_type.name
+            return token.typ
 
 
 class Scanner:
-    def __init__(self, source: str):
-        self.source = source
-        self.start = 0
-        self.current = 0
-        self.tokens: List[Token] = []
+    def __init__(self):
+        self._restart('')
 
-    def scan(self):
-        while(not self.is_at_end()):
-            self.scan_one_token()
-            self.start = self.current
-        return self.tokens
+    def scan(self, source: str):
+        self._restart(source)
+        while(not self._is_at_end()):
+            self._scan_one_token()
+            self._start = self._current
+        return self._tokens
 
-    def scan_one_token(self):
-        c = self.advance()
+    def _restart(self, source: str):
+        self._source = source
+        self._start = 0
+        self._current = 0
+        self._tokens: List[Token] = []
+
+    def _scan_one_token(self):
+        c = self._advance()
         if c == '(':
-            self.add_token(TokenType.LEFT_PAREN)
+            self._add_token(LEFT_PAREN)
         elif c == ')':
-            self.add_token(TokenType.RIGHT_PAREN)
+            self._add_token(RIGHT_PAREN)
         elif c == '\'':
-            self.scan_quote()
+            self._scan_quote()
         elif c == '"':
-            self.scan_string()
+            self._scan_string()
         elif not c.isspace():
-            self.scan_nonstring()
+            self._scan_nonstring()
 
-    def is_at_end(self):
-        return self.current >= len(self.source)
+    def _is_at_end(self):
+        return self._current >= len(self._source)
 
-    def advance(self):
-        c = self.source[self.current]
-        self.current += 1
+    def _advance(self):
+        c = self._source[self._current]
+        self._current += 1
         return c
 
-    def peek(self):
-        return self.source[self.current]
+    def _peek(self):
+        return self._source[self._current]
 
-    def add_token(self, token_type: TokenType, literal=None):
-        lexeme = self.source[self.start:self.current]
-        self.tokens.append(Token(token_type, lexeme, literal))
+    def _add_token(self, typ: str, literal=None):
+        lexeme = self._source[self._start:self._current]
+        self._tokens.append(Token(typ, lexeme, literal))
 
-    def scan_quote(self):
-        if self.is_at_end():
+    def _scan_quote(self):
+        if self._is_at_end():
             panic('scanner: quote should not be at the end')
-        elif self.peek().isspace():
+        elif self._peek().isspace():
             panic('scanner: quote should not be followed by space')
         else:
-            self.add_token(TokenType.QUOTE)
+            self._add_token(QUOTE)
 
-    def scan_string(self):
-        while not self.is_at_end() and self.peek() != '"':
-            self.advance()
+    def _scan_string(self):
+        while not self._is_at_end() and self._peek() != '"':
+            self._advance()
 
-        if self.is_at_end():
+        if self._is_at_end():
             panic('scanner: unterminated string: %s' %
-                  self.source[self.start:self.current])
+                  self._source[self._start:self._current])
 
         # consume ending "
-        self.advance()
+        self._advance()
 
         # trim the surrounding quotes
-        lexemem = self.source[self.start+1:self.current-1]
-        self.add_token(TokenType.STRING, lexemem)
+        lexemem = self._source[self._start+1:self._current-1]
+        self._add_token(STRING, lexemem)
 
-    def scan_nonstring(self):
-        while not self.is_at_end() and not Scanner.can_terminate_nonstring(self.peek()):
-            c = self.advance()
+    def _scan_nonstring(self):
+        while not self._is_at_end() and not Scanner._can_terminate_nonstring(self._peek()):
+            c = self._advance()
             if c == '\'' or c == '"':
                 panic('scanner: invalid quote: %s' %
-                      self.source[self.start:self.current])
+                      self._source[self._start:self._current])
 
         try:
-            lexemem = float(self.source[self.start:self.current])
-            self.add_token(TokenType.NUMBER, lexemem)
+            lexemem = float(self._source[self._start:self._current])
+            self._add_token(NUMBER, lexemem)
         except:
-            lexemem = self.source[self.start:self.current]
+            lexemem = self._source[self._start:self._current]
             if lexemem[0].isnumeric():
                 panic('scanner: symbol should not start with number: %s' % lexemem)
-            self.add_token(TokenType.SYMBOL, lexemem)
+            self._add_token(SYMBOL, lexemem)
 
     @staticmethod
-    def can_terminate_nonstring(c: str):
+    def _can_terminate_nonstring(c: str):
         return c.isspace() or c == '(' or c == ')'
 
 
@@ -145,46 +148,52 @@ class Scanner:
 # empty list is represented as expression of type list but value none
 # ref: https://craftinginterpreters.com/parsing-expressions.html
 
-@enum.unique
-class ExprType(enum.Enum):
-    LIST = enum.auto()
-    SYMBOL = enum.auto()
-    NUMBER = enum.auto()
-    STRING = enum.auto()
-
+LIST = 'LIST'
 
 class Expression:
-    def __init__(self, expr_type: ExprType, value):
-        self.expr_type = expr_type
+    def __init__(self, typ: str, value):
+        self.typ = typ
         self.value = value
 
-    def stringify(self) -> str:
-        if self.expr_type == ExprType.SYMBOL:
-            return self.value
-        elif self.expr_type == ExprType.NUMBER:
-            return stringify_number(self.value)
-        elif self.expr_type == ExprType.STRING:
-            return '"%s"' % self.value
-        else:
+
+class ExprPrinter:
+    def stringify(self, expr: Expression) -> str:
+        if expr.typ == SYMBOL:
+            return expr.value
+        elif expr.typ == NUMBER:
+            return stringify_number(expr.value)
+        elif expr.typ == STRING:
+            return '"%s"' % expr.value
+        elif expr.typ == LIST:
             substrs: List[str] = []
-            head: Optional[LinkedList] = self.value
+            head: Optional[LinkedList] = expr.value
             while head is not None:
-                substrs.append(head.value.stringify())
+                substrs.append(self.stringify(head.expr))
                 head = head.next
             return "(%s)" % (" ".join(substrs))
+        else:
+            panic('expr printer: unknown type = %s' % expr.typ)
 
 
 class LinkedList:
-    def __init__(self, value: Expression, next=None):
-        self.value = value
+    def __init__(self, expr: Expression, next: Optional["LinkedList"] = None):
+        self.expr = expr
         self.next = next
 
     @staticmethod
-    def from_list(values: List[Expression]):
+    def from_list(expr_list: List[Expression]):
         head = None
-        for i in range(len(values)-1, -1, -1):
-            head = LinkedList(values[i], head)
+        for i in range(len(expr_list)-1, -1, -1):
+            head = LinkedList(expr_list[i], head)
         return head
+
+    def length(self):
+        count = 0
+        head = self
+        while head:
+            count += 1
+            head = head.next
+        return count
 
 
 class Parser:
@@ -194,84 +203,182 @@ class Parser:
     list -> LEFT_PAREN ( expression )* RIGHT_PAREN;
     '''
 
-    def __init__(self, tokens: List[Token]):
-        self.tokens = tokens
-        self.current = 0
+    def __init__(self):
         self.lookup_table = {
-            TokenType.SYMBOL: self.parse_symbol,
-            TokenType.NUMBER: self.parse_number,
-            TokenType.STRING: self.parse_string,
-            TokenType.QUOTE: self.parse_quote,
-            TokenType.LEFT_PAREN: self.parse_left_paren,
-            TokenType.RIGHT_PAREN: self.parse_right_paren
+            SYMBOL: self._parse_simple,
+            NUMBER: self._parse_simple,
+            STRING: self._parse_simple,
+            QUOTE: self._parse_quote,
+            LEFT_PAREN: self._parse_left_paren,
+            RIGHT_PAREN: self._parse_right_paren
         }
+        self._restart([])
+        self._token_printer = TokenPrintier()
 
-    def parse(self):
+    def parse(self, tokens: List[Token]):
+        self._restart(tokens)
         if len(self.tokens) == 0:
             return None
-        expr = self.parse_recursive()
-        if not self.is_at_end():
-            panic('parser: excessive tokens: %s' % self.tokens[self.current].stringify())
+        expr = self._parse_recursive()
+        if not self._is_at_end():
+            panic('parser: excessive tokens: %s' %
+                  self._token_printer.stringify(self.tokens[self.current]))
         return expr
 
-    def parse_recursive(self) -> Expression:
-        if self.is_at_end():
+    def _restart(self, tokens: List[Token]):
+        self.tokens = tokens
+        self.current = 0
+
+    def _parse_recursive(self) -> Expression:
+        if self._is_at_end():
             panic('parser: recursive parse failed')
-        token = self.advance()
-        return self.lookup_table[token.token_type](token)
+        token = self._advance()
+        return self.lookup_table[token.typ](token)
 
-    def parse_symbol(self, token: Token):
-        return Expression(ExprType.SYMBOL, token.literal)
+    def _parse_simple(self, token: Token):
+        return Expression(token.typ, token.literal)
 
-    def parse_number(self, token: Token):
-        return Expression(ExprType.NUMBER, token.literal)
+    def _parse_quote(self, _token: Token):
+        expr_list = []
+        expr_list.append(Expression(SYMBOL, 'quote'))
+        expr_list.append(self._parse_recursive())
+        return Expression(LIST, LinkedList.from_list(expr_list))
 
-    def parse_string(self, token: Token):
-        return Expression(ExprType.STRING, token.literal)
-
-    def parse_quote(self, _token: Token):
-        values = []
-        values.append(Expression(ExprType.SYMBOL, 'quote'))
-        values.append(self.parse_recursive())
-        return Expression(ExprType.LIST, LinkedList.from_list(values))
-
-    def parse_left_paren(self, _token: Token):
-        values = []
-        while not self.is_at_end() and self.peek().token_type != TokenType.RIGHT_PAREN:
-            values.append(self.parse_recursive())
-        if self.is_at_end() or self.peek().token_type != TokenType.RIGHT_PAREN:
+    def _parse_left_paren(self, _token: Token):
+        expr_list = []
+        while not self._is_at_end() and self._peek().typ != RIGHT_PAREN:
+            expr_list.append(self._parse_recursive())
+        if self._is_at_end() or self._peek().typ != RIGHT_PAREN:
             panic('parser: list missing right parenthesis')
-        self.advance()  # consume right parenthesis
-        return Expression(ExprType.LIST, LinkedList.from_list(values))
+        self._advance()  # consume right parenthesis
+        return Expression(LIST, LinkedList.from_list(expr_list))
 
-    def parse_right_paren(self, _token: Token):
+    def _parse_right_paren(self, _token: Token):
         panic('parser: unmatched right parenthesis')
 
-    def is_at_end(self):
+    def _is_at_end(self):
         return self.current >= len(self.tokens)
 
-    def advance(self):
+    def _advance(self):
         tk = self.tokens[self.current]
         self.current += 1
         return tk
 
-    def peek(self):
+    def _peek(self):
         return self.tokens[self.current]
 
+
+# scheme analyzer
+# we assume operator cannot be a procedure that evaluates to symbol
+# see 4.1.7
+
+
+class Analyzer:
+    def __init__(self, special_forms: Dict[str, Callable], procedure: Callable, symbol: Callable):
+        self._special_forms = special_forms
+        self._procedure = procedure
+        self._symbol = symbol
+
+    def analyze(self, expr: Optional[Expression]):
+        if expr is not None:
+            if expr.typ == NUMBER or expr.typ == STRING:
+                return lambda env: expr
+            elif expr.typ == SYMBOL:
+                return self.analyze_symbol(expr)
+            else:  # must be list
+                return self.analyze_list(expr)
+
+    def analyze_symbol(self, expr: Expression):
+        return self._symbol(expr, self.analyze)
+
+    def analyze_list(self, expr: Expression):
+        head: Optional[LinkedList] = expr.value
+        if head is None:  # empty list
+            panic('analyzer: cannot evaluate empty list')
+        else:
+            hexpr = head.expr
+            if hexpr.typ == NUMBER:
+                panic('analyzer: number cannot be operator or special form')
+            if hexpr.typ == STRING:
+                panic('analyzer: string cannot be operator or special form')
+            if hexpr.typ == SYMBOL:
+                f = self._special_forms.get(hexpr.value, None)
+                if f is not None:
+                    return f(expr, self.analyze)
+            return self._procedure(expr, self.analyze)
+
+
+class Procedure:
+    def __init__(self, parameters: List[str], body: Callable):
+        self.parameters = parameters
+        self.body = body
+
+
+def analyze_symbol(expr: Expression, _analyze: Callable):
+    '''lookup variable'''
+    name = expr.value
+    return lambda env: env.lookup(name)
+
+
+def analyze_quote(expr: Expression, _analyze: Callable):
+    head: LinkedList = expr.value
+    if head.length() == 2:
+        quoted = head.next.expr
+        return lambda env: quoted
+    else:
+        panic('analyze_quote: list length should be 2')
+
+
+def analyze_set(expr: Expression, analyze: Callable):
+    head: LinkedList = expr.value
+    name: str = head.expr.value
+    if head.length() == 2:
+        get_value = analyze(head.next)
+        return lambda env: env.set(name, get_value())
+    else:
+        panic('analyze_set: list length should be 2')
+    
+
+def analyze_define(expr: Expression, analyze: Callable):
+    head: LinkedList = expr.value
+    name: str = head.expr.value
+    hl = head.length()
+    if hl == 2:
+        # define variable
+        get_expr = analyze(head.next)
+        return lambda env: env.define(name, get_expr())
+    elif hl == 3:
+        # define procedure
+        if head.next.expr.typ != LIST:
+            panic('analyze_define: procedure parameters must be a list')
+        parameters: List[str] = []
+        para_exprs: LinkedList = head.next.expr.value
+        while para_exprs is not None:
+            if para_exprs.expr.typ != SYMBOL:
+                panic('analyze_define: procedure parameters must all be symbols')
+            parameters.append(para_exprs.expr.value)
+            para_exprs = para_exprs.next
+        body = analyze(head.next.next.expr)
+        proc = Procedure(parameters, body)
+        return lambda env: env.define(name, proc)
+    else:
+        panic('analyze_define: list length should be 2')
+    
 
 def test_one(source: str, token_str_expect: Optional[str], expr_str_expect: Optional[str]):
     # source
     print('source: %s' % source)
     # scan
-    scanner = Scanner(source)
-    tokens = scanner.scan()
-    token_str = ', '.join([t.stringify() for t in tokens])
+    scanner = Scanner()
+    tokens = scanner.scan(source)
+    token_stringifier = TokenPrintier()
+    token_str = ', '.join([token_stringifier.stringify(t) for t in tokens])
     print('tokens: %s' % token_str)
     if token_str_expect is not None:
         assert token_str == token_str_expect
     # parse
-    parser = Parser(tokens)
-    expr = parser.parse()
+    parser = Parser()
+    expr = parser.parse(tokens)
     expr_str = expr.stringify() if expr else ''
     print('expression: %s' % expr_str)
     if expr_str_expect is not None:
